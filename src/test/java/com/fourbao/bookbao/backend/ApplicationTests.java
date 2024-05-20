ad3c3aad8d47a6a178d5438a165f70512311f0d1
BOK-MINJEONG <rena6296@gmail.com>
2024-05-20 18:47:51 +0900

    Initialize project

---
+++ src/test/java/com/fourbao/bookbao/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourbao.bookbao.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
